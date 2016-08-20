f3565583f31bdcdfd9be32a376b62a47d25b358d
Oleg verhovsky <verchol@gmail.com>
2016-08-20 19:16:33 +0300

    add

--- output/watcher.js
+++ output/watcher.js
@@ -4,9 +4,12 @@ var watcher = filewatcher();
 
 
 // ... or a directory
+
+console.log(`watching on folder ${process.env.OUTPUT_DIR}`);
+
 watcher.add(process.env.OUTPUT_DIR);
 
 watcher.on('change', function(file, stat) {
-  console.log('File modified: %s', file);
+  console.log('File modified: %s %j', file, stat);
   if (!stat) console.log('deleted');
 });
